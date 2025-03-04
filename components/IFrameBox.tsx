67f658598f49e85b56c5830ce023c4ee47b853b9
nguyen-anthony <nguyen.tonyphuc@gmail.com>
2025-03-03 21:37:01 -0600

    mobile friendly

--- components/IFrameBox.tsx
+++ components/IFrameBox.tsx
@@ -10,8 +10,12 @@ const IFrameBox: React.FC<IFrameBoxProps> = ({ url }) => {
     <Box
       sx={{
         position: "relative",
-        width: "850px",
-        height: "640px",
+        width: { xs: "100%", md: "850px" },
+        height: {
+          xs: "calc(100vh - 220px)", // nearly full screen on mobile
+          md: "calc(100vh - 200px)", // fixed height on desktop
+        },
+        paddingTop: { xs: "75%", md: 0 },
         overflow: "hidden",
         borderRadius: 2,
         boxShadow: 3,
@@ -22,6 +26,9 @@ const IFrameBox: React.FC<IFrameBoxProps> = ({ url }) => {
         src={url}
         title="Example Iframe"
         style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
           width: "100%",
           height: "100%",
           border: "none",
--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -9,26 +9,57 @@ import {
   MenuItem,
   Box,
   IconButton,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  useTheme,
+  useMediaQuery,
 } from "@mui/material";
-import { SiInstagram, SiX, SiYoutube, SiTwitch, SiBluesky, SiTiktok } from "react-icons/si";
+import MenuIcon from "@mui/icons-material/Menu";
+import {
+  SiInstagram,
+  SiX,
+  SiYoutube,
+  SiTwitch,
+  SiBluesky,
+  SiTiktok,
+} from "react-icons/si";
 
 const Navigation: React.FC = () => {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const theme = useTheme();
+  // `isMobile` will be true if the viewport width is at or below the "sm" breakpoint
+  const menuBreakpoint = useMediaQuery(theme.breakpoints.down("md"));
+  const socialMediaBreakpoint = useMediaQuery(theme.breakpoints.down("sm"));
 
   const handleMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
   };
 
   const handleMenuClose = () => {
-    setTimeout(() => {
-      setAnchorEl(null);
-    }, 50);
+    setAnchorEl(null);
   };
+
+  const toggleDrawer = () => {
+    setDrawerOpen((prev) => !prev);
+  };
+
+  const navItems = [
+    { title: "Home", href: "/" },
+    { title: "About Me", href: "/about" },
+  ];
+
+  const legacyChallenges = [
+    { title: "Career Legacy Challenge", href: "/Sims4/careerlegacychallenge" },
+    { title: "Llamas & Legends Legacy", href: "/Sims4/llamaslegendslegacy" },
+  ];
 
   return (
     <AppBar position="sticky" sx={{ backgroundColor: "black" }}>
       <Toolbar>
-        {/* Left side: Social media icons and title */}
+        {/* Left side: Title + Conditional Social Icons */}
         <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
           <Typography
             variant="h6"
@@ -38,89 +69,132 @@ const Navigation: React.FC = () => {
           >
             Risshella
           </Typography>
-          <IconButton
-            color="inherit"
-            component="a"
-            href="https://twitch.tv/Risshella"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <SiTwitch size={24} />
-          </IconButton>
-          <IconButton
-            color="inherit"
-            component="a"
-            href="https://youtube.com/Risshella"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <SiYoutube size={24} />
-          </IconButton>
-          <IconButton
-            color="inherit"
-            component="a"
-            href="https://bsky.app/profile/risshella.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <SiBluesky size={24} />
-          </IconButton>
-          <IconButton
-            color="inherit"
-            component="a"
-            href="https://instagram.com/Risshella"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <SiInstagram size={24} />
-          </IconButton>
-          <IconButton
-            color="inherit"
-            component="a"
-            href="https://tiktok.com/@Risshella"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <SiTiktok size={24} />
-          </IconButton>
-          <IconButton
-            color="inherit"
-            component="a"
-            href="https://x.com/Risshellala"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <SiX size={24} />
-          </IconButton>
+
+          {/* Only show icons if not on mobile */}
+          {!socialMediaBreakpoint && (
+            <>
+              <IconButton
+                color="inherit"
+                component="a"
+                href="https://twitch.tv/Risshella"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <SiTwitch size={24} />
+              </IconButton>
+              <IconButton
+                color="inherit"
+                component="a"
+                href="https://youtube.com/Risshella"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <SiYoutube size={24} />
+              </IconButton>
+              <IconButton
+                color="inherit"
+                component="a"
+                href="https://bsky.app/profile/risshella.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <SiBluesky size={24} />
+              </IconButton>
+              <IconButton
+                color="inherit"
+                component="a"
+                href="https://instagram.com/Risshella"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <SiInstagram size={24} />
+              </IconButton>
+              <IconButton
+                color="inherit"
+                component="a"
+                href="https://tiktok.com/@Risshella"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <SiTiktok size={24} />
+              </IconButton>
+              <IconButton
+                color="inherit"
+                component="a"
+                href="https://x.com/Risshellala"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <SiX size={24} />
+              </IconButton>
+            </>
+          )}
         </Box>
 
-        {/* Right side: Navigation links */}
-        <Box sx={{ display: "flex", gap: 2, marginLeft: "auto" }}>
-          <Button color="inherit" href="/">
-            Home
-          </Button>
-          <Button color="inherit" href="/about">
-            About Me
-          </Button>
-          <Button color="inherit" onMouseEnter={handleMenuOpen} onClick={handleMenuOpen}>
-            Legacy Challenges
-          </Button>
-          <Menu
-            anchorEl={anchorEl}
-            open={Boolean(anchorEl)}
-            onClose={handleMenuClose}
-            MenuListProps={{ onMouseLeave: handleMenuClose }}
-            sx={{ mt: 1 }}
+        {/* Right side: Navigation links or hamburger menu */}
+        {menuBreakpoint ? (
+          <IconButton
+            color="inherit"
+            edge="end"
+            onClick={toggleDrawer}
+            sx={{ marginLeft: "auto" }}
           >
-            <MenuItem onClick={handleMenuClose} component="a" href="/Sims4/careerlegacychallenge">
-              Career Legacy Challenge
-            </MenuItem>
-            <MenuItem onClick={handleMenuClose} component="a" href="/Sims4/llamaslegendslegacy">
-              Llamas &amp; Legends Legacy
-            </MenuItem>
-          </Menu>
-        </Box>
+            <MenuIcon />
+          </IconButton>
+        ) : (
+          <Box sx={{ display: "flex", gap: 2, marginLeft: "auto" }}>
+            {navItems.map((item) => (
+              <Button key={item.title} color="inherit" href={item.href}>
+                {item.title}
+              </Button>
+            ))}
+            <Button color="inherit" onClick={handleMenuOpen}>
+              Legacy Challenges
+            </Button>
+            <Menu
+              anchorEl={anchorEl}
+              open={Boolean(anchorEl)}
+              onClose={handleMenuClose}
+            >
+              {legacyChallenges.map((challenge) => (
+                <MenuItem
+                  key={challenge.title}
+                  onClick={handleMenuClose}
+                  component="a"
+                  href={challenge.href}
+                >
+                  {challenge.title}
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        )}
       </Toolbar>
+
+      {/* Mobile Drawer */}
+      <Drawer anchor="right" open={drawerOpen} onClose={toggleDrawer}>
+        <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer}>
+          <List>
+            {navItems.map((item) => (
+              <ListItem key={item.title} component="a" href={item.href}>
+                <ListItemText primary={item.title} />
+              </ListItem>
+            ))}
+            <ListItem onClick={handleMenuOpen}>
+              <ListItemText primary="Legacy Challenges" />
+            </ListItem>
+            {legacyChallenges.map((challenge) => (
+              <ListItem
+                key={challenge.title}
+                component="a"
+                href={challenge.href}
+              >
+                <ListItemText primary={challenge.title} />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Drawer>
     </AppBar>
   );
 };
