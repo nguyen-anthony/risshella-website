a92613e067600ff9dabe735b2b3d01811bb17374
nguyen-anthony <nguyen.tonyphuc@gmail.com>
2025-03-22 19:54:57 -0500

    keep the url. just rewrite

--- next.config.js
+++ next.config.js
@@ -1,19 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'careerlegacychallenge.risshella.com',
-          },
-        ],
-        destination: 'https://www.risshella.com/sims4/careerlegacychallenge/:path*',
-        permanent: true,
-      },
-    ];
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/:path*',
+          has: [
+            {
+              type: 'host',
+              value: 'careerlegacychallenge.risshella.com',
+            },
+          ],
+          destination: '/sims4/careerlegacychallenge/:path*',
+        },
+      ],
+    };
   },
 }
 
