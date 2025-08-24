d17454b10a237c152ea72e6d23a54d04d0568c96
nguyen-anthony <nguyen.tonyphuc@gmail.com>
2025-08-24 18:20:13 -0500

    add links to legacy challenge on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,53 @@
+"use client";
+
 import styles from "./page.module.css";
 import * as React from 'react';
 import Navigation from '../components/Navigation';
-
+import { Typography, Button, Stack, Container } from '@mui/material';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <div>
       <Navigation/>
-      <main className={styles.main}>
-        <h1>Under Construction</h1>
-        <p>
-          Check back soon for more content.
-        </p>
-      </main>
+      <Container component="main" sx={{ mt: 8, mb: 8 }}>
+        <Stack spacing={4} alignItems="center">
+          <Typography variant="h2" component="h1" gutterBottom align="center">
+            Welcome to Risshella&apos;s Website
+          </Typography>
+          <Typography variant="body1" component="p" gutterBottom align="center">
+            Under Construction
+          </Typography>
+          <Typography variant="body1" component="p" gutterBottom align="center">
+            In the meantime, check out some of the legacy challenges below!
+          </Typography>
+          <Stack spacing={3} alignItems="center">
+            <Typography variant="h4" component="h2" gutterBottom align="center">
+              Sims 4 Legacy Challenges
+            </Typography>
+            <Stack spacing={2} alignItems="center">
+              <Button 
+                variant="contained" 
+                size="large"
+                onClick={() => router.push('/sims4/careerlegacychallenge')}
+                sx={{ minWidth: 250 }}
+              >
+                Career Legacy Challenge
+              </Button>
+              <Button 
+                variant="contained" 
+                size="large"
+                onClick={() => router.push('/sims4/llamaslegendslegacy')}
+                sx={{ minWidth: 250 }}
+              >
+                Llamas Legends Legacy
+              </Button>
+            </Stack>
+          </Stack>
+        </Stack>
+      </Container>
       <footer className={styles.footer}>
         
       </footer>
