a81ad0102485066c5dd40052b360464b520ade80
Tony <53980868+nguyen-anthony@users.noreply.github.com>
2025-11-20 22:22:19 -0600

    dark mode navigation (#59)

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,25 @@ import * as React from "react";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { CacheProvider } from "@emotion/react";
 import createEmotionCache from "@/utils/createEmotionCache";
-import theme from "@/utils/theme";
+import { createAppTheme } from "@/utils/theme";
 import IssueReportButton from "@/components/common/IssueReportButton";
 import { usePathname } from "next/navigation";
 import { Analytics } from "@vercel/analytics/next";
+import { ThemeProvider as CustomThemeProvider, useThemeMode } from "@/utils/ThemeContext";
 
 const clientSideEmotionCache = createEmotionCache();
+
+function ThemeWrapper({ children }: { children: React.ReactNode }) {
+  const { mode } = useThemeMode();
+  const theme = createAppTheme(mode);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  );
+}
 
 export default function RootLayout({
   children,
@@ -23,12 +36,13 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <CacheProvider value={clientSideEmotionCache}>
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
-            {children}
-            {!isOverlay && <IssueReportButton />}
-            <Analytics />
-          </ThemeProvider>
+          <CustomThemeProvider>
+            <ThemeWrapper>
+              {children}
+              {!isOverlay && <IssueReportButton />}
+              <Analytics />
+            </ThemeWrapper>
+          </CustomThemeProvider>
         </CacheProvider>
       </body>
     </html>
