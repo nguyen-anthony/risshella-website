64a96fcebdd0c5db2f2b96d65889090c954580d1
nguyen-anthony <nguyen.tonyphuc@gmail.com>
2025-11-23 21:58:33 -0600

    remove navigation from overlay

--- app/villagerhunt/layout.tsx
+++ app/villagerhunt/layout.tsx
@@ -1,17 +1,21 @@
 "use client";
 import ChangelogNotification from '@/components/villagerhunt/ChangelogNotification';
 import Navigation from '@/components/common/Navigation';
+import { usePathname } from 'next/navigation';
 
 export default function VillagerHuntLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+  const isOverlay = pathname?.includes('/overlay');
+
   return (
     <>
-      <Navigation />
+      {!isOverlay && <Navigation />}
       {children}
-      <ChangelogNotification />
+      {!isOverlay && <ChangelogNotification />}
     </>
   );
 }
